a0e8050d727c4bfe93acb59a00ee3adfd8ae4a18
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-02-14 15:10:40 +0000

    DDCE-5852 - inital wip commit - bootstrap changes

--- app/connectors/TrustsConnector.scala
+++ app/connectors/TrustsConnector.scala
@@ -19,77 +19,91 @@ package connectors
 import config.AppConfig
 import models.http.TaxableMigrationFlag
 import models.{MigratingTrustDetails, NonMigratingTrustDetails, TrustDetailsType}
+import play.api.libs.json.Json
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class TrustsConnector @Inject()(http: HttpClient, config: AppConfig) {
+class TrustsConnector @Inject()(http: HttpClientV2, config: AppConfig) {
 
   private val trustsUrl: String = s"${config.trustsUrl}/trusts"
   private val baseUrl: String = s"$trustsUrl/trust-details"
 
   def getTrustDetails(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[TrustDetailsType] = {
-    val url = s"$baseUrl/$identifier/transformed"
-    http.GET[TrustDetailsType](url)
+    val fullUrl = s"$baseUrl/$identifier/transformed"
+    http.get(url"$fullUrl").execute[TrustDetailsType]
+    //http.GET[TrustDetailsType](url)
   }
 
   def setUkProperty(identifier: String, value: Boolean)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/uk-property"
-    http.PUT[Boolean, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/uk-property"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+    //http.PUT[Boolean, HttpResponse](url, value)
   }
 
   def setTrustRecorded(identifier: String, value: Boolean)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/recorded"
-    http.PUT[Boolean, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/recorded"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+//    http.PUT[Boolean, HttpResponse](url, value)
   }
 
   def setUkRelation(identifier: String, value: Boolean)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/uk-relation"
-    http.PUT[Boolean, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/uk-relation"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+   // http.PUT[Boolean, HttpResponse](url, value)
   }
 
   def setUkResident(identifier: String, value: Boolean)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/uk-resident"
-    http.PUT[Boolean, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/uk-resident"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+   // http.PUT[Boolean, HttpResponse](url, value)
   }
 
   def getTrustMigrationFlag(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[TaxableMigrationFlag] = {
-    val url = s"$trustsUrl/$identifier/taxable-migration/migrating-to-taxable"
-    http.GET[TaxableMigrationFlag](url)
+    val fullUrl = s"$trustsUrl/$identifier/taxable-migration/migrating-to-taxable"
+    http.get(url"$fullUrl").execute[TaxableMigrationFlag]
+    //http.GET[TaxableMigrationFlag](url)
   }
 
   def setMigratingTrustDetails(identifier: String, value: MigratingTrustDetails)
                               (implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/migrating-trust-details"
-    http.PUT[MigratingTrustDetails, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/migrating-trust-details"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+   // http.PUT[MigratingTrustDetails, HttpResponse](url, value)
   }
 
   def setNonMigratingTrustDetails(identifier: String, value: NonMigratingTrustDetails)
                                  (implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/non-migrating-trust-details"
-    http.PUT[NonMigratingTrustDetails, HttpResponse](url, value)
+    val fullUrl = s"$baseUrl/$identifier/non-migrating-trust-details"
+    http.put(url"$fullUrl").withBody(Json.toJson(value)).execute[HttpResponse]
+    //http.PUT[NonMigratingTrustDetails, HttpResponse](url, value)
   }
 
   def wasTrustRegisteredWithDeceasedSettlor(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[Boolean] = {
-    val url = s"$baseUrl/$identifier/has-deceased-settlor"
-    http.GET[Boolean](url)
+    val fullUrl = s"$baseUrl/$identifier/has-deceased-settlor"
+    http.get(url"$fullUrl").execute[Boolean]
+   // http.GET[Boolean](url)
   }
 
   def getTrustName(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[String] = {
-    val url = s"$baseUrl/$identifier/trust-name"
-    http.GET[String](url)
+    val fullUrl = s"$baseUrl/$identifier/trust-name"
+    http.get(url"$fullUrl").execute[String]
+   // http.GET[String](url)
   }
 
   def removeTrustTypeDependentTransformFields(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$trustsUrl/$identifier/trust-type-dependent-transform-fields"
-    http.DELETE[HttpResponse](url)
+    val fullUrl = s"$trustsUrl/$identifier/trust-type-dependent-transform-fields"
+    http.delete(url"$fullUrl").execute[HttpResponse]
+    //http.DELETE[HttpResponse](url)
   }
 
   def removeOptionalTrustDetailTransforms(identifier: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[HttpResponse] = {
-    val url = s"$baseUrl/$identifier/optional-trust-detail-transforms"
-    http.DELETE[HttpResponse](url)
+    val fullUrl = s"$baseUrl/$identifier/optional-trust-detail-transforms"
+    http.delete(url"$fullUrl").execute[HttpResponse]
+   // http.DELETE[HttpResponse](url)
   }
 
 }
